aca56732e5e594c54adb1d92cc9f3e866805a67e
Gabriel Cuvillier <contact@gabrielcuvillier.pro>
2020-03-12 15:49:09 +0100

    MSVC fix

--- Source/FreeImage/Plugin.cpp
+++ Source/FreeImage/Plugin.cpp
@@ -24,8 +24,7 @@
 // =====================================================================
 
 #ifdef _MSC_VER
-#pragma warning(
-    disable : 4786) // identifier was truncated to 'number' characters
+#pragma warning(disable : 4786) // identifier was truncated to 'number' characters
 #endif
 
 #ifdef _WIN32
