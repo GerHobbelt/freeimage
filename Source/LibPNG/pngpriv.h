5db2755e95c75b4e8ae9e54e97d10d8b33994b20
Markus Schweitzer <markus.schweitzer@technoteam.de>
2024-09-19 08:38:24 +0200

    Update pngpriv.h

--- Source/LibPNG/pngpriv.h
+++ Source/LibPNG/pngpriv.h
@@ -522,7 +522,7 @@
     * <fp.h> if possible.
     */
 #    if !defined(__MATH_H__) && !defined(__MATH_H) && !defined(__cmath__)
-#      include <fp.h>
+//#      include <fp.h>
 #    endif
 #  else
 #    include <math.h>
